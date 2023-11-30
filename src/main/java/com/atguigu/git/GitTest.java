22bf110633e053d07b4ac07e94c20c5a512c364a
alec <alec@qq.com>
2023-11-30 23:17:54 +0800

    idea git 学习

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author alec
+ * @date 2023/11/30 23:14
+ * @description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
